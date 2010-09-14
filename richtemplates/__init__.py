874ec6ca17c749f62dc26c97e7d234f09dcab248
Lukasz Balcerzak <lukasz.balcerzak@python-center.org>
2010-09-15 01:35:53 +0200

    Bumped version

--- richtemplates/__init__.py
+++ richtemplates/__init__.py
@@ -5,7 +5,7 @@ Richfaces library offers for J2EE developers - not outstanding but still nice
 looking and elegant.
 """
 
-__version__ = '0.3.5'
+__version__ = '0.3.6'
 
 def get_version():
     return __version__
